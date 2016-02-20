5fcfd8ca65df9767a989a339f836136e979d259b
Runzhi Yang <yangr@rose-hulman.edu>
2016-02-20 11:33:01 -0500

    Working on framework

--- app/src/main/java/backEndApi/framework/AbstractPost.java
+++ app/src/main/java/backEndApi/framework/AbstractPost.java
@@ -1,7 +1,24 @@
 package backEndApi.framework;
 
+import java.util.Observable;
+
 /**
  * Created by yangr on 2/20/2016.
  */
-public class AbstractPost {
+public class AbstractPost extends Observable implements IPost {
+
+    @Override
+    public void reply(IUser author, String content) {
+
+    }
+
+    @Override
+    public void bid(IUser bidder, int amount) {
+
+    }
+
+    @Override
+    public void pay(IUser buyer, IUser seller) {
+
+    }
 }
