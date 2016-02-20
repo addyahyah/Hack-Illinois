65569cd822e4bcce2d3b33455155c63621989ae1
Matthew Persing <persinme@rose-hulman.edu>
2016-02-20 17:25:31 -0600

    Adding userreplies relationship class for azure.
    Should be the last of the data models!

---
+++ app/src/main/java/azurecomm/data/UserReplies.java
@@ -0,0 +1,33 @@
+package azurecomm.data;
+
+/**
+ * Created by persinme on 2/20/2016.
+ */
+public class UserReplies {
+
+    @com.google.gson.annotations.SerializedName("id")
+    private String mId;
+
+    @com.google.gson.annotations.SerializedName("ReplyID")
+    private String mReplyId;
+
+    @com.google.gson.annotations.SerializedName("FacebookID")
+    private String mUserId;
+
+    public UserReplies() {}
+
+    public UserReplies(String id, String replyId, String postId) {
+        this.setId(id);
+        this.setReplyId(replyId);
+        this.setUserId(postId);
+    }
+
+    public void setId(String id) { this.mId = id; }
+    public String getId() { return this.mId; }
+
+    public void setUserId(String userId) { this.mUserId = userId; }
+    public String getUserId() { return this.mUserId; }
+
+    public void setReplyId(String replyId) { this.mReplyId = replyId; }
+    public String getReplyId() { return this.mReplyId; }
+}
