c233dde9dc3618d3c12c72c6b0f6542eaba2acd8
Runzhi Yang <novosejr@rose-hulman.edu>
2016-02-20 14:34:16 -0500

    Changed the layout of the app, creating the structure of the app (not complete yet)

---
+++ app/src/main/java/com/example/hackillinois0219/PostAdapter.java
@@ -0,0 +1,45 @@
+package com.example.hackillinois0219;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Joe on 2/20/2016.
+ */
+public class PostAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+
+    public PostAdapter(){
+
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    protected class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+
+        }
+
+
+    }
+
+}
