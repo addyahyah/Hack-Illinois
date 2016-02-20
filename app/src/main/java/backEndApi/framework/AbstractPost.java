5b6f9bfeb2833b77b8449d4d113af73af5f1edf8
Matthew Persing <persinme@rose-hulman.edu>
2016-02-20 05:04:08 -0600

    WIP

--- app/src/main/java/backEndApi/framework/AbstractPost.java
+++ app/src/main/java/backEndApi/framework/AbstractPost.java
@@ -1,7 +1,47 @@
 package backEndApi.framework;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+
 /**
  * Created by yangr on 2/20/2016.
  */
-public class AbstractPost {
+public class AbstractPost extends Observable implements IPost {
+    private final int barginTime = 180; //in seconds
+    protected IUser author;
+    protected IUser seller;
+    protected String description;
+    protected double price;
+    protected List<IReply> replies;
+    protected Date creationTime;
+    protected Date expirationTime;
+    protected boolean hasTransactionComplete;
+
+    public AbstractPost(Map<String, Object> postMap){
+        this.author = (IUser)postMap.get("author");
+        this.description = (String) postMap.get("description");
+        this.price = (double) postMap.get("price");
+        this.seller = (IUser) postMap.get("seller");
+        this.replies = (List<IReply>) postMap.get("replies");
+        this.creationTime = (Date) postMap.get("creationDate");
+        this.expirationTime = (Date) postMap.get("expirationTime");
+        this.hasTransactionComplete = (boolean) postMap.get("hasTransactionComplete");
+    }
+
+    @Override
+    public void reply(IUser author, String content) {
+
+    }
+
+    @Override
+    public void bid(IUser bidder, int amount) {
+
+    }
+
+    @Override
+    public void pay(IUser buyer, IUser seller) {
+
+    }
 }
